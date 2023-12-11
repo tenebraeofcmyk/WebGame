7cb7ca83d553e55b3cae9eca63df377eaeb6fac2
Ashe <115732638+tenebraeofcmyk@users.noreply.github.com>
2023-12-11 10:50:27 -0600

    crank lever logic needed

--- js/drawbridge.js
+++ js/drawbridge.js
@@ -12,99 +12,108 @@ let correctAnswersJournal = localStorage.getItem('correctAnswersJournal') || 0;
 document.getElementById('correctCountJournal').textContent = correctAnswersJournal;
 
 //script lines
-window.onload = (event) => {
-   console.log("page is fully loaded");
 
-    const story = {
-        startDrawbridge: {
-            lines: [
-                "You begin to realize that you have no memory of your whereabouts before waking up in this castle.",
-                "You wonder if you will be able recall your memories if you leave the castle."
-            ]
-        },
-        crankLever: {
-            lines: [
-                "You see there is a lever you need to crank if you will have any successful attempt to open the drawbridge."
-            ]
-        },
-        leaveCastle: {
-            lines: [
-                "You succeed, the lever is smooth and pulls the gear so that you can now open the drawbridge.",
-                "You step outside, and you are met with the same cloudy sky like in the courtyard.",
-                "There is a large forest in front of you.",
-            ]
-        },
-        encounterWolves: {
-            lines: [
-                "You step forward, trying to remember anything.",
-                "As you move forward, red eyes appear in front of you in the shadows.",
-                "You hear growls from the forest, growing closer each second.",
-            ]
 
-        },
-        drawbridgePuzzle: {
-            lines: [
-                "You rush back into the castle, and you try to shut the drawbridge."
+const story = {
+    opening: {
+        lines: [
+            ""
+        ]
+    },
+    startDrawbridge: {
+        lines: [
+            "You begin to realize that you have no memory of your whereabouts before waking up in this castle.",
+            "You wonder if you will be able recall your memories if you leave the castle."
+        ]
+    },
+    crankLever: {
+        lines: [
+            "You see there is a lever you need to crank if you will have any successful attempt to open the drawbridge."
+        ]
+    },
+    leaveCastle: {
+        lines: [
+            "You succeed, the lever is smooth and pulls the gear so that you can now open the drawbridge.",
+            "You step outside, and you are met with the same cloudy sky like in the courtyard.",
+            "There is a large forest in front of you.",
+        ]
+    },
+    encounterWolves: {
+        lines: [
+            "You step forward, trying to remember anything.",
+            "As you move forward, red eyes appear in front of you in the shadows.",
+            "You hear growls from the forest, growing closer each second.",
+        ]
 
-            ]
-        },
-    }
+    },
+    drawbridgePuzzle: {
+        lines: [
+            "You rush back into the castle, and you try to shut the drawbridge."
 
-let currentChapter = "startDrawbridge";
+        ]
+    },
+}
 
-    function displayChapter() {
-        console.log('Entering displayChapter. Current chapter:', currentChapter);
-        const storyContainer = document.getElementById('scriptContainer');
-        const chapter = story[currentChapter];
+let currentChapter = "opening";
 
-        const chapterElement = document.createElement('div');
+function displayChapter() {
+    console.log('Entering displayChapter. Current chapter:', currentChapter);
+    const storyContainer = document.getElementById('scriptContainer');
+    const chapter = story[currentChapter];
 
-        chapter.lines.forEach(line => {
-            const lineElement = document.createElement('p');
-            lineElement.classList.add('lead', 'my-3');
-            lineElement.textContent = line;
-            chapterElement.appendChild(lineElement);
-        });
-        storyContainer.innerHTML = '';
-        storyContainer.appendChild(chapterElement);
+    const chapterElement = document.createElement('div');
 
-    }
+    chapter.lines.forEach(line => {
+        const lineElement = document.createElement('p');
+        lineElement.classList.add('lead', 'my-3');
+        lineElement.textContent = line;
+        chapterElement.appendChild(lineElement);
+    });
+    storyContainer.innerHTML = '';
+    storyContainer.appendChild(chapterElement);
 
-    document.getElementById('nextButton').addEventListener('click', nextChapter);
+}
 
-    const drawbridgeContainer = document.getElementById("drawbridgeContainer");
-    document.getElementById('startDrawbridgeB').addEventListener('click', startDrawbridge);
+document.getElementById('nextButton').addEventListener('click', nextChapter);
 
-    const leverContainer = document.getElementById("leverContainer");
-    document.getElementById('crankLeverB').addEventListener('click', crankLever);
+const drawbridgeContainer = document.getElementById("drawbridgeContainer");
+document.getElementById('startDrawbridgeB').addEventListener('click', nextChapter);
 
-    const leaveContainer = document.getElementById("leaveContainer");
-    document.getElementById('leaveCastleB').addEventListener('click', leaveCastle);
+const leverContainer = document.getElementById("leverContainer");
+document.getElementById('crankLeverB').addEventListener('click', nextChapter);
 
-    const wolvesContainer = document.getElementById("wolvesContainer");
-    document.getElementById('encounterWolvesB').addEventListener('click', encounterWolves);
+const leaveContainer = document.getElementById("leaveContainer");
+document.getElementById('leaveCastleB').addEventListener('click', leaveCastle);
 
-    const logicContainer = document.getElementById("logicContainer");
-    document.getElementById("drawbridgePuzzleB").addEventListener('click', drawbridgePuzzle);
+const wolvesContainer = document.getElementById("wolvesContainer");
+document.getElementById('encounterWolvesB').addEventListener('click', encounterWolves);
 
-   function nextChapter() {
+const logicContainer = document.getElementById("logicContainer");
+document.getElementById("drawbridgePuzzleB").addEventListener('click', drawbridgePuzzle);
+
+function nextChapter() {
     // Logic to determine the next chapter based on the current chapter
     console.log('Entering nextChapter. Current chapter:', currentChapter);
 
     switch (currentChapter) {
-        case "startDrawbridge":
-            //console.log('Switch case: startDrawbridge');
-            drawbridgeContainer.style.display = 'block';
-            nextButton.style.display = "none";
-            startDrawbridgeB.style.display = "block";
+        case "opening":
             currentChapter = "startDrawbridge";
+            drawbridgeContainer.style.display = 'block';
+            nextButton.style.display ="none";
+            break;
+        case "startDrawbridge":
+            console.log('Switch case: startDrawbridge');
+            nextButton.style.display = "none";
+            drawbridgeContainer.style.display = 'none';
+            // startDrawbridgeB.style.display = "none";
+            currentChapter = "crankLever"; 
+            leverContainer.style.display = "block";
             break;
         case "crankLever":
             //console.log('Switch case: crankLever');
-            leverContainer.style.display = 'block';
+            leverContainer.style.display = 'none';
             nextButton.style.display = "none";
-            crankLeverB.style.display = "block";
-            currentChapter = "crankLever";
+           
             break;
         case "leaveCastle":
             //console.log('Switch case: leaveCastle');
@@ -133,17 +142,7 @@ let currentChapter = "startDrawbridge";
     document.getElementById('nextButton').addEventListener('click', nextChapter);
 }
 
-// Load correct answer count from local storage on page load
-let correctAnswersJewel = localStorage.getItem('correctAnswersJewel') || 0;
-document.getElementById('correctCountJewel').textContent = correctAnswersJewel;
-let correctAnswersFood = localStorage.getItem('correctAnswersFood') || 0;
-document.getElementById('correctCountFood').textContent = correctAnswersFood;
-let correctAnswersCoins = localStorage.getItem('correctAnswersCoins') || 0;
-document.getElementById('correctCountCoins').textContent = correctAnswersCoins;
-let correctAnswersTorch = localStorage.getItem('correctAnswersTorch') || 0;
-document.getElementById('correctCountTorch').textContent = correctAnswersTorch;
-let correctAnswersIronPick = localStorage.getItem('correctAnswersIronPick') || 0;
-document.getElementById('correctCountIronPick').textContent = correctAnswersIronPick;
+
 
 document.getElementById("goToNextRoom").style.display = "none";
 
@@ -164,12 +163,12 @@ function drawbridgePuzzle() {
     // Initial Display
     document.getElementById("result").innerHTML = '';
     document.getElementById("drawbridgePuzzle1B").style.display = "none";
-    document.getElementById("userAnswer").value='3';
+    document.getElementById("userAnswer").value = '3';
     document.getElementById("enterCourtyard").style.display = "none";
 
     // Equation    
     const correctAnswerdrawbridge = 3;
-    
+
     // Problem and userInput visible
     document.getElementById("problem").innerHTML = `There are two wolves in front of a wolf, two wolves behind a duck and a wolf in the middle. How many wolves are there?`;
     document.getElementById("userInput").style.display = "block";
@@ -177,23 +176,23 @@ function drawbridgePuzzle() {
     // Check  answer when the user submits
     document.getElementById("submitB").onclick = function () {
         const userAnswer = document.getElementById("userAnswer").value;
-    
+
         if (userAnswer == correctAnswerDrawbridge) {
-            
+
             document.getElementById("result").innerHTML = "Correct! The drawbridge cranks open, and you rush inside before the wolves can bite you!";
             correctAnswersJewel++;
-            
+
             // Save correct answer count to local storage
             localStorage.setItem('correctAnswersJewel', correctAnswersJewel);
             updateCorrectCountJewel();
 
             document.getElementById("goToNextRoom").style.display = "block";
             document.getElementById("labyrinthPuzzle1B").style.display = "none";
-        } 
+        }
 
         else {
-                document.getElementById("result").innerHTML = `The wolves get closer. You try again.`;
-                document.getElementById("labyrinthPuzzle1B").style.display = "block";
+            document.getElementById("result").innerHTML = `The wolves get closer. You try again.`;
+            document.getElementById("labyrinthPuzzle1B").style.display = "block";
         }
 
         // Hide the input field after submitting
@@ -203,24 +202,24 @@ function drawbridgePuzzle() {
         function updateCorrectCountJewel() {
             document.getElementById('correctCountJewel').textContent = correctAnswersJewel;
         }
-        }  
     }
+}
 
-        function updateCorrectCountFood() {
-            document.getElementById('correctCountFood').textContent = correctAnswersFood;
-        }
-        }  
+function updateCorrectCountFood() {
+    document.getElementById('correctCountFood').textContent = correctAnswersFood;
+}
+
 
 
 function clearLocalStorage() {
     var confirmation = confirm("Restarting the game will erase all your inventory. Do you want to restart?");
-    
+
     if (confirmation) {
         // User clicked "Yes"
         localStorage.clear();
-        window.location.href = 'myindex.html'; 
-    } 
+        window.location.href = 'myindex.html';
+    }
     else {
         // User clicked "No" or closed the dialog
-    } 
+    }
 }
